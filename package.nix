ed566fdbc9456e853c6547c283443016e4f7d5a0
anathema <garmir@pm.me>
2026-09-20 19:25:30 +0100

    follow the reworked window fit on the garmir branch

--- package.nix
+++ package.nix
@@ -12,7 +12,7 @@ let
   # the gui is built from the garmir branch of the fork, which merges
   # encoder rotation on the matrix tester (vial-kb/vial-gui#400) and a
   # keyboard drawing that shrinks to fit the window.
-  rev = "6fe7b9882bd2971747643c7bd53e90cc0d6584be";
+  rev = "f365727553340597a90f598c1362e82b66171849";
 
   python = python3.withPackages (
     ps: with ps; [
@@ -32,7 +32,7 @@ stdenv.mkDerivation (finalAttrs: {
     owner = "garmir";
     repo = "vial-gui";
     inherit rev;
-    hash = "sha256-yrSyg9XA2y8FF1g4w4ltFD4y9STScULiEbOlc26XVO4=";
+    hash = "sha256-YmkbQt9nEzSw2Ly1iMgJS+lzd+YE6xUONEF+tCy/O68=";
   };
 
   nativeBuildInputs = [
